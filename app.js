ec6c915686f5f89b00344f89249d255a1a0000e7
analuzalbornoz <analuzalbornoz@gmail.com>
2022-01-05 12:51:26 -0300

    app.init

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.set('port', process.env.PORT || 3000)
+
+app.listen(app.get('port'), () => {
+    console.log('Servidor funcionando en el puerto ' + app.get('port'));
+})
+
+
+console.log(__dirname);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html')
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html')
+});
